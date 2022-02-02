92607982931c1c36848c21d5d88c6762cf3621a1
Khuirul-Huda <hudah2102@gmail.com>
2022-02-02 17:57:11 +0700

    idk

--- main.dart
+++ main.dart
@@ -1,2 +1,17 @@
-void main() {
+import 'dart:convert';
+import 'dart:io';
+
+Future<void> main() async {
+  String result = await httpget("http://localhost:631");
+  print(result);
+}
+
+Future<String> httpget(String url) async {
+  HttpClient client = new HttpClient();
+  client.badCertificateCallback =
+      (X509Certificate cert, String host, int port) => true;
+  HttpClientRequest request = await client.getUrl(Uri.parse(url));
+  HttpClientResponse response = await request.close();
+  String reply = await response.transform(utf8.decoder).join();
+  return reply;
 }
